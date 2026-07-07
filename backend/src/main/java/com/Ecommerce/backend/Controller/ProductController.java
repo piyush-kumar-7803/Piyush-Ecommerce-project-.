83179619e44dbf748183d9d26672eff7a00dc285
Piyush Kumar <piyushkumar7803@gmail.com>
2026-07-07 12:45:15 +0530

    Initial commit

---
+++ backend/src/main/java/com/Ecommerce/backend/Controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.Ecommerce.backend.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class ProductController {
+
+    @GetMapping("/")
+    public String greet(){
+        return "started mate";
+    }
+}
