c94fa04771c29a2ff456ad57db176ffe86f08331
Piyush Kumar <piyushkumar7803@gmail.com>
2026-07-07 15:46:32 +0530

    linked three repo service and controller

--- backend/src/main/java/com/Ecommerce/backend/Controller/ProductController.java
+++ backend/src/main/java/com/Ecommerce/backend/Controller/ProductController.java
@@ -1,9 +1,18 @@
 package com.Ecommerce.backend.Controller;
 
+import com.Ecommerce.backend.service.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
+
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
 
     @GetMapping("/")
     public String greet(){
--- backend/src/main/java/com/Ecommerce/backend/service/ProductService.java
+++ backend/src/main/java/com/Ecommerce/backend/service/ProductService.java
@@ -1,8 +1,19 @@
 package com.Ecommerce.backend.service;
 
+
+import com.Ecommerce.backend.repo.ProductRepository;
+
 import org.springframework.stereotype.Service;
+
+
 
 @Service
 public class ProductService {
+private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
 
 }
