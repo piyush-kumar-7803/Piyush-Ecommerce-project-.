be78287a56cec88f45bb7fb6431fc3bd5b8da162
Piyush Kumar <piyushkumar7803@gmail.com>
2026-08-07 15:48:18 +0530

    added JwtService

--- backend/src/main/java/com/Ecommerce/backend/Security/JwtService.java
+++ backend/src/main/java/com/Ecommerce/backend/Security/JwtService.java
@@ -1,4 +1,92 @@
 package com.Ecommerce.backend.Security;
 
-public class JwtService {
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {// Use a long random secret in a real project.
+    private static final String SECRET_KEY =
+            "my-super-secret-key-for-ecommerce-jwt-authentication-123456789";
+
+    // Token validity: 24 hours
+    private static final long EXPIRATION_TIME =
+            1000 * 60 * 60 * 24;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(
+                SECRET_KEY.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    // Generate JWT
+    public String generateToken(UserDetails userDetails) {
+
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(
+                        new Date(System.currentTimeMillis()
+                                + EXPIRATION_TIME)
+                )
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    // Extract username/email from JWT
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    // Extract any claim
+    public <T> T extractClaim(
+            String token,
+            Function<Claims, T> claimsResolver) {
+
+        Claims claims = extractAllClaims(token);
+
+        return claimsResolver.apply(claims);
+    }
+
+    // Extract all claims
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    // Check whether token is expired
+    private boolean isTokenExpired(String token) {
+
+        return extractExpiration(token)
+                .before(new Date());
+    }
+
+    // Get expiration date
+    private Date extractExpiration(String token) {
+
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    // Validate token
+    public boolean isTokenValid(
+            String token,
+            UserDetails userDetails) {
+
+        String username = extractUsername(token);
+
+        return username.equals(userDetails.getUsername())
+                && !isTokenExpired(token);
+    }
 }
