fefed70b04cbdf9dcf1e4bb40576ad13f21dbaf7
Piyush Kumar <piyushkumar7803@gmail.com>
2026-08-07 15:53:30 +0530

    added Login Request DTO

--- backend/src/main/java/com/Ecommerce/backend/Dto/Auth/LoginRequest.java
+++ backend/src/main/java/com/Ecommerce/backend/Dto/Auth/LoginRequest.java
@@ -1,4 +1,20 @@
 package com.Ecommerce.backend.Dto.Auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
 }
