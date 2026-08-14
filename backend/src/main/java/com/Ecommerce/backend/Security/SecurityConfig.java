8dc441b3d140df648f470194182290b05662a6ca
Piyush Kumar <piyushkumar7803@gmail.com>
2026-08-14 15:02:19 +0530

    deployed on vercel and render

--- backend/src/main/java/com/Ecommerce/backend/Security/SecurityConfig.java
+++ backend/src/main/java/com/Ecommerce/backend/Security/SecurityConfig.java
@@ -79,7 +79,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://piyush-ecommerce-project-m1j96eays-piyush-5814.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
